706d783e4210aaf1f2e6bfac0253646fd3fefa84
joshuawuro <joshuawuro@gmail.com>
2025-06-02 10:47:15 +0000

    add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-
       colors: {
-        blue: '#1E305E',
-        dark_blue: '#0D162A',
-        brown: '#CD9F70',
-      }
+        blue: "#1E305E",
+        dark_blue: "#0D162A",
+        brown: "#CD9F70",
+        gray_background: "#E9EAEF",
+        statusError: "#DC3534",
+        borderSubtle: "#d1d5db",
+        textPlaceholder: "#A9B0B7",
+        inputBorder: "#D1D5DB",
+      },
     },
   },
   plugins: [],
-}
+};
