bf8d7ea8ba528fdea5af629d2f4aa3d9a939bc97
TINUNGTAA <tinungtaa@gmail.com>
2025-06-03 08:46:39 -1200

    fix: conflict resolved

--- src/components/pages/Trust.tsx
+++ src/components/pages/Trust.tsx
@@ -5,61 +5,41 @@ import Footer from "../layout/Footer";
 const Trust = () => {
   return (
     <div className="font-inter">
-      <div>
-        <HeroLayout backgroundImage={assets.trust.bg6}>
-          <div className="text-white text-center">
-            <h1 className="text-6xl font-bold  ">
-              How Trust <br />
-              Is
-              <span className="text-brown px-3">Built</span>{" "}
-            </h1>
-          </div>
-        </HeroLayout>
-        <div className="w-2/3 mx-auto py-20">
-          <p className=" text-center font-normal text-[32px]">
-            At Honest Builders, we know your home is the most significant
-            investment you'll ever make. Whether this is your first time or your
-            fifth, you're the reason we've built our business on the foundation
-            of honesty and transparency.
-          </p>
-        </div>
-    <div>
       <HeroLayout backgroundImage={assets.trust.bg6}>
         <div className="text-white text-center">
-          <h1 className="text-[64px] font-bold  ">
+          <h1 className="text-[64px] font-bold">
             How Trust <br />
             Is
-            <span className="text-brown px-3">Built</span>{" "}
+            <span className="text-brown px-3">Built</span>
           </h1>
         </div>
       </HeroLayout>
+
       <div className="w-2/3 mx-auto py-20">
-        <p className=" text-center text-[32px]">
+        <p className="text-center font-normal text-[32px]">
           At Honest Builders, we know your home is the most significant
           investment you'll ever make. Whether this is your first time or your
-          fifth, you're the reason we've built our business on the foundation of
-          honesty and transparency.
+          fifth, you're the reason we've built our business on the foundation
+          of honesty and transparency.
         </p>
       </div>
 
-      <div className=" flex  px-28 py-16 items-center  justify-between bg-blue ">
-        <div className=" flex-col  w-1/5 ">
+      <div className="flex px-28 py-16 items-center justify-between bg-blue">
+        <div className="flex-col w-1/5">
           <img src={assets.trust.review} />
-
-          <p className="  text-white  font-medium  text-center text-3xl">
+          <p className="text-white font-medium text-center text-3xl">
             See reviews from our satisfied customers
           </p>
         </div>
 
-        <div className=" flex  gap-12">
-          <div className="  w-[440px] h-[470px]  py-12  rounded-3xl shadow-lg  bg-[#DDE0E7]">
-            {/* Inner Slate Box */}
-            <div className="flex flex-col items-center rounded-xl border-brown border bg-[#DDE0E7] gap-6 px-6  w-[90%]  mx-auto  text-center">
+        <div className="flex gap-12">
+          <div className="w-[440px] h-[470px] py-12 rounded-3xl shadow-lg bg-[#DDE0E7]">
+            <div className="flex flex-col items-center rounded-xl border-brown border bg-[#DDE0E7] gap-6 px-6 w-[90%] mx-auto text-center">
               <p className="text-[32px] mt-3 text-center font-extrabold">
                 Top Quality <br />
                 <img src={assets.trust.stars} className="mx-auto" />
               </p>
-              <p className="text-2xl  font-normal text-center">
+              <p className="text-2xl font-normal text-center">
                 Everyone, and I mean everyone; from the managers, to the worker
                 and non-staff were very professional. The house is so gorgeous
                 my wife can't stop gushing over it. Quality is out of this
@@ -68,15 +48,14 @@ const Trust = () => {
               <p className="font-semibold">Efisah. J</p>
             </div>
           </div>
-          {/* box2 */}
-          <div className="  w-[440px] h-[470px]  py-12  rounded-3xl shadow-lg  bg-[#DDE0E7]">
-            {/* Inner Slate Box */}
-            <div className="flex flex-col items-center rounded-xl border-brown border bg-[#DDE0E7] gap-6 px-6  w-[90%]  mx-auto  text-center">
+          
+          <div className="w-[440px] h-[470px] py-12 rounded-3xl shadow-lg bg-[#DDE0E7]">
+            <div className="flex flex-col items-center rounded-xl border-brown border bg-[#DDE0E7] gap-6 px-6 w-[90%] mx-auto text-center">
               <p className="text-[32px] mt-3 text-center font-extrabold">
                 Top Quality <br />
                 <img src={assets.trust.stars} className="mx-auto" />
               </p>
-              <p className="text-2xl  font-normal text-center">
+              <p className="text-2xl font-normal text-center">
                 Everyone, and I mean everyone; from the managers, to the worker
                 and non-staff were very professional. The house is so gorgeous
                 my wife can't stop gushing over it. Quality is out of this
@@ -88,9 +67,9 @@ const Trust = () => {
         </div>
       </div>
 
-      <div className="w-4/5 mx-auto  py-20">
-        <p className=" text-center font-normal text-[32px] mb-12">
-          “The heart of man is deceitful above all things,” and trust is hard to
+      <div className="w-4/5 mx-auto py-20">
+        <p className="text-center font-normal text-[32px] mb-12">
+          "The heart of man is deceitful above all things," and trust is hard to
           find, so we've dedicated ourselves to doing things right by you. Your
           peace of mind is very important to us, so we act with integrity and
           trustworthiness as core values in our relationship, and we stress the
@@ -99,31 +78,21 @@ const Trust = () => {
         </p>
       </div>
 
-      <div className=" flex rounded-full mx-auto items-center w-[680px] h-[680px]  mb-16  border-brown border-[5px]  bg-gray-500">
-        <div className=" flex-col items-center justify-center mx-auto w-[654px] h-[654px]   border-brown border-[5px]  rounded-full bg-blue space-y-6 px-24 py-12">
+      <div className="flex rounded-full mx-auto items-center w-[680px] h-[680px] mb-16 border-brown border-[5px] bg-gray-500">
+        <div className="flex-col items-center justify-center mx-auto w-[654px] h-[654px] border-brown border-[5px] rounded-full bg-blue space-y-6 px-24 py-12">
           <img src={assets.trust.badge} className="mx-auto" />
           <img src={assets.home.logo} className="mx-auto" />
-
           <p className="text-[32px] font-medium text-white text-center">
             We've made trust our cornerstone by building it in to everything we
             do, brick by brick and day after day.
           </p>
-          <img src={assets.trust.badge} className="mx-auto" />
-          <img src={assets.home.logo} className="mx-auto" />
-
-          <p className="text-[32px] font-medium text-white text-center">We've made trust our cornerstone by building it
-            in to everything we do,
-            brick by brick and day after day.</p>
-
-
         </div>
       </div>
 
       <div className="flex p-28 justify-between items-center bg-[#B9BFCD]">
-        <div className="flex flex-col items-start  w-1/3">
+        <div className="flex flex-col items-start w-1/3">
           <img src={assets.trust.house} className="w-2/5 mx-auto" />
           <div className="mx-auto">
-            {" "}
             <p className="text-[32px] font-medium text-center text-[#172447] mt-6">
               Floor plans designed for
               <br />
@@ -131,26 +100,24 @@ const Trust = () => {
             </p>
           </div>
         </div>
-
         <div>
           <img src={assets.trust.live} />
         </div>
       </div>
 
-      <div className="flex items-center justify-between gap-[160px] p-28  ">
+      <div className="flex items-center justify-between gap-[160px] p-28">
         <div className="flex w-3/5">
           <img src={assets.trust.eff} />
         </div>
-
-        <div className=" flex flex-col flex-1 text-center  text-[32px] text-[#172447]">
-          <img src={assets.trust.energy} className="h-[300px] " />
-          <p className=" font-medium mt-6">A dedication to energy</p>
-          <p className=" font-bold">EFFICIENCY</p>
+        <div className="flex flex-col flex-1 text-center text-[32px] text-[#172447]">
+          <img src={assets.trust.energy} className="h-[300px]" />
+          <p className="font-medium mt-6">A dedication to energy</p>
+          <p className="font-bold">EFFICIENCY</p>
         </div>
       </div>
 
       <div className="flex p-28 justify-between items-center bg-[#B9BFCD]">
-        <div className="flex flex-col items-start  w-1/3">
+        <div className="flex flex-col items-start w-1/3">
           <img src={assets.trust.star} className="w-2/5 mx-auto" />
           <div className="mx-auto">
             <p className="text-[32px] font-medium text-center text-[#172447] mt-6">
@@ -159,18 +126,16 @@ const Trust = () => {
             </p>
           </div>
         </div>
-
         <div>
           <img src={assets.trust.dist} />
         </div>
       </div>
 
       <div className="py-16">
-        <div className="w-[894px] h-[412px] mx-auto py-6 rounded-[30px]  shadow-[2px_4px_15px_0px_#00000040]  bg-[#DDE0E7]">
-          {/* Inner Slate Box */}
-          <div className="flex flex-col items-center rounded-[30px]  border-brown border bg-[#DDE0E7] gap-12 px-16 py-8 w-[93%] mx-auto  text-center">
+        <div className="w-[894px] h-[412px] mx-auto py-6 rounded-[30px] shadow-[2px_4px_15px_0px_#00000040] bg-[#DDE0E7]">
+          <div className="flex flex-col items-center rounded-[30px] border-brown border bg-[#DDE0E7] gap-12 px-16 py-8 w-[93%] mx-auto text-center">
             <p className="text-[48px] font-semibold">
-              MAKE AN INFORMED <span className="text-brown ">CHOICE</span>{" "}
+              MAKE AN INFORMED <span className="text-brown">CHOICE</span>
             </p>
             <p className="text-[20px] font-normal">
               We back ourselves up to deliver and bring the best out of
